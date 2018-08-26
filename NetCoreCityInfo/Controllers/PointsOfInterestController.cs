bf367d31491a257dc1752efbf69454ed15afe7b8
Alberto A <alberto.arpini@fmc-ag.com>
2018-08-26 19:28:51 +0200

    added controller

---
+++ NetCoreCityInfo/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreCityInfo.Models;
+
+namespace NetCoreCityInfo.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{id}", Name = "GetPointOfInterest")]
+        public IActionResult GetPointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pointOfInterest);
+        }
+
+        // Aggiunge una risorsa
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointsOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if(pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            // Per gestire errori personalizzati, va popolato il model state secondo le proprie logiche
+            // Ovviamente non è il massimo, perchè la validazione è fatta un po' nel DTO e un po' nel controller
+            // Nel corso si suggerisce di usare FluentValidation
+            if(pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "Description must be different than name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            // in pratica recupera il route template GetPointOfInterest, che abbiamo definito un paio di metodi prima, e 
+            // valorizza i parmetri cityId e id con un oggetto anonimo
+            // Infine mette finalPointOfInterest nel body
+            return CreatedAtRoute("GetPointOfInterest", new { cityId, id = finalPointOfInterest.Id }, finalPointOfInterest);
+        }
+
+        // Aggiorna una risorsa
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id,
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            // Per gestire errori personalizzati, va popolato il model state secondo le proprie logiche
+            // Ovviamente non è il massimo, perchè la validazione è fatta un po' nel DTO e un po' nel controller
+            // Nel corso si suggerisce di usare FluentValidation
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "Description must be different than name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            return NoContent();
+        }
+
+        // Specifiche delle operazioni Json patch: https://tools.ietf.org/html/rfc6902
+        [HttpPatch("{cityId}/pointsofinterest/{id}")]
+        public IActionResult PartialUpdatePointOfInterest(int cityId, int id,
+            [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if(patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch =
+                new PointOfInterestForUpdateDto
+                {
+                    Description = pointOfInterestFromStore.Description,
+                    Name = pointOfInterestFromStore.Name
+                };
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            
+            if(pointOfInterestToPatch.Description == pointOfInterestToPatch.Name)
+            {
+                ModelState.AddModelError("Description", "The provided description should be different than the name");
+            }
+
+            // Va chiamato a mano
+            // perchè il framewrok valida il parametro in ingresso: JsonPatchDocument
+            // e non PointOfInterestForUpdateDto, quindi le data annotations vanno a farsi benedire
+            TryValidateModel(pointOfInterestToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterestToPatch.Name;
+            pointOfInterestFromStore.Description = pointOfInterestToPatch.Description;
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{cityId}/pointsofinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
+        }
+
+        // test
+        [HttpPost("load")]
+        public IActionResult TextXml([FromBody]XElement o)
+        {
+            var attr =
+                from el in o.Elements("content")
+                where (string)el.Attribute("a") == "xxx"
+                select el;
+
+            foreach (XElement el in attr)
+                Console.WriteLine(el);
+
+            return Ok();
+        }
+    }
+}
--- NetCoreCityInfo/Startup.cs
+++ NetCoreCityInfo/Startup.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace NetCoreCityInfo
 {
@@ -25,7 +27,19 @@ namespace NetCoreCityInfo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(o => {
+                    // se vogliamo che i nomi delle proprietà json siano uguali alle classi c# 
+                    // (altrimenti newtonsoft applicherà il pascalCase
+                    //var castedResover = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                    //castedResover.NamingStrategy = null;
+                })
+                .AddMvcOptions(o => {
+                    // Se vogliamo il risultato in Json
+                    //o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                    o.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+                })
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
