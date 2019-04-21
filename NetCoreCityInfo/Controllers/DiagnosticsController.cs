556cb722dedb742b37ea5755a51e07759eab522a
Alberto A <alberto.arpini@fmc-ag.com>
2019-04-21 14:56:45 +0200

    added dignostics

---
+++ NetCoreCityInfo/Controllers/DiagnosticsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreCityInfo.Controllers
+{
+    [Route("api/diagnostics")]
+    public class DiagnosticsController : Controller
+    {
+        IHostingEnvironment _env;
+
+        public DiagnosticsController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+        [HttpGet()]
+        public IActionResult GetDiagnostics()
+        {
+            return Ok(_env.EnvironmentName);
+        }
+    }
+}
